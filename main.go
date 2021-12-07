7577408ca70b8a378303d83fad4db60ab5ddf2b6
Mike Watkins <github@mikewatkins.ca>
2021-12-07 03:25:10 -0800

    Disable cache-control by default

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 // httphere provides a simple http file server for ad-hoc use, testing HTML, etc.
-//
-// The package also provides an example for publishing to github, versioning, and configuration via the flag package.
+// Given the intended use, cache-control headers are disabled by middleware by default.
 //
 // usage:
-//   httphere
+//   httphere -h
 //
-// httphere attempt to start the server listening on port 8080; if the port is
-// already in use, it will iterate +1 port at a time until 8099.
+// httphere attempts to bind to the specified/default port; if the port can't be bound to,
+// it will attempt to bind to another by iterating through up to 100 ports.
 
 package main
 
 import (
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"strings"
@@ -21,17 +21,19 @@ import (
 	"github.com/go-chi/chi/middleware"
 )
 
-const nPorts int = 100
+const tryPorts int = 100
 
 var (
-	version     string = "development" // set via go build --ldflags, see Makefile
+	version     string = "1.0.1"
 	httpAddress string = "0.0.0.0"
 	httpPort    int    = 8080
+	noCache     bool   = true
 )
 
 func init() {
 	flag.StringVar(&httpAddress, "address", httpAddress, "Address server should listen on")
 	flag.IntVar(&httpPort, "port", httpPort, "Port server should bind to")
+	flag.BoolVar(&noCache, "nocache", noCache, "Sets no-cache and other headers.\n-nocache=false to disable")
 }
 
 func main() {
@@ -39,25 +41,34 @@ func main() {
 	flag.Parse()
 
 	r := chi.NewRouter()
-	r.Use(middleware.NoCache)
+	if noCache {
+		r.Use(middleware.NoCache)
+	}
 	r.Use(middleware.Logger)
+
+	// FileServer setup
 	workdir, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 	FileServer(r, "/", http.Dir(workdir))
 
-	for i := httpPort; i <= httpPort+nPorts; i++ {
+	// net.Listen to port or try range of ports if not able to bind
+	for i := httpPort; i <= httpPort+tryPorts; i++ {
 		addr = fmt.Sprintf("%s:%d", httpAddress, i)
-		fmt.Printf("httphere (%s) listening at: %s, serving files from: %s\n", version, addr, workdir)
-		err = http.ListenAndServe(addr, r)
+		l, err := net.Listen("tcp", addr)
 		if err != nil {
-			fmt.Printf("Error: %s - trying next.\n", err)
+			fmt.Printf("Error: %s - trying next port.\n", err)
 			continue
+		}
+		fmt.Printf("httphere (%s) listening at %s, serving files from %s\n", version, addr, workdir)
+		err = http.Serve(l, r)
+		if err != nil {
+			panic(err)
 		}
 		break
 	}
-	fmt.Printf("No available ports between %d and %d.", httpPort, httpPort+nPorts)
+	fmt.Printf("Fatal: No bind-able ports between %d and %d.\n", httpPort, httpPort+tryPorts)
 }
 
 // FileServer sets up a http.FileServer handler for an http.FileSystem.
